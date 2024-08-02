119fa3eed778ff17621afd8297fb5f5de7b48e30
harshil <harshil.y.joshi.2004@gmail.com>
2024-08-02 19:48:12 +0530

    Angular Practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { UserListComponent } from "./user-list/user-list.component";
+import { LoginComponent } from './login/login.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserListComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
